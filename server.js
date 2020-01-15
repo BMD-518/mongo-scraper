18450e3326d6eadfd6f1d4dd6bcbd5780c95cb20
Bijan Dayani <bdayani518@gmail.com>
2020-01-15 12:16:01 -0500

    clean up, require routes folder

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ PORT = process.env.PORT || 3000;
 
 var app = express();
 
-// app('views', path.join(__dirname, 'views'));
-// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-// app.set('view engine', 'handlebars');
-
+// Set views path
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs( 
     {defaultLayout: 'main'}
@@ -28,6 +25,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(express.static('public'));
 
+require('./routes/');
 
 app.listen(PORT, function() {
     console.log('App running on port ' + PORT + '!');
