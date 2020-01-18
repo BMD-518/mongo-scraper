2bfc10c0201254bb120b8104c16c1b670d2d22aa
Bijan Dayani <bdayani518@gmail.com>
2020-01-17 21:11:50 -0500

    put routes into server.js file

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-require('dotenv').config()
+require('dotenv').config();
 var express = require('express');
-var path = require('path');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
 var exphbs = require("express-handlebars");
+var path = require('path');
+
 var axios = require('axios');
 var cheerio = require('cheerio');
-var mongoose = require('mongoose');
 
 PORT = process.env.PORT || 3000;
 
@@ -17,10 +20,11 @@ var app = express();
 
 // Set views path
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs( 
-    {defaultLayout: 'main'}
+app.engine('handlebars', exphbs(
+    { defaultLayout: 'main' }
 ));
 app.set('view engine', 'handlebars');
+app.use(logger('dev'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(express.static('public'));
@@ -34,18 +38,57 @@ var MONGO_URI = 'mongodb://localhost/scraper_db';
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
-.then( () => console.log('DB Connected!'))
-.catch(err => console.log(err));
+}).then(() => console.log('DB Connected!'))
+    .catch(err => console.log(err));
 
 // testing home route for handlebars
 app.get('/', (req, res) => {
-    res.render('home');
+    // res.render('home');
+});
+
+app.get('/scrape', (req, res) => {
+    axios.get('https://www.huffpost.com/news/politics').then((response) => {
+        var $ = cheerio.load(response.data);
+        console.log(res);
+
+        // target each element with class of "card__text"
+        $('.card__text').each( function(i, element) {
+
+            // empty results object
+            var result = {};
+
+            // target text from all .card__headlines elements for title
+            result.title = $(this).children('.card__headlines').text();
+            
+            // target text from all .card__description elements for summary
+            result.summary = $(this).children('.card__description').text();
+
+            // target href of .card__headline from child a tag
+            result.link = $(this).children('.card__headlines').children('a').attr('href');
+
+            result.saved = false;
+
+            // console.log(result);
+
+            // Create new Articles from each result
+
+            db.Article.create(result).then((dbArticle) => {
+                console.log(dbArticle);
+            }).catch(err => console.log(err));
+            
+        });
+        // redirect to home route after scraping
+        res.redirect('/');
+    });
 });
 
 
 
 
-app.listen(PORT, function() {
+
+
+
+
+app.listen(PORT, function () {
     console.log('App running on port ' + PORT + '!');
-  });
+});
